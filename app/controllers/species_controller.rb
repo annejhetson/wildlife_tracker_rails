3df14e144c88a1616b3fd728aa60a20ebd07c0ad
anne jhetson & Kelly Becker <student@epicodus.com>
2014-04-01 11:51:02 -0700

    completes CRUD for species

---
+++ app/controllers/species_controller.rb
@@ -0,0 +1,43 @@
+class SpeciesController < ApplicationController
+  def index
+    @species = Species.all
+    render('species/index.html.erb')
+  end
+
+  def new
+    @animal = Species.new()
+    render('species/new.html.erb')
+  end
+
+  def create
+    @animal = Species.new(:name => params[:name])
+    if @animal.save
+      render('species/success.html.erb')
+    else
+      render('species/new.html.erb')
+    end
+  end
+
+  def show
+    @animal = Species.find(params[:id])
+    render('species/show.html.erb')
+  end
+
+  def destroy
+    @animal = Species.find(params[:id])
+    @animal.destroy
+    render('species/delete.html.erb')
+  end
+
+  def update
+    @species = Species.all
+    @animal = Species.find(params[:id])
+    @animal.update(:name => params[:name])
+    render('species/index.html.erb')
+  end
+
+  def edit
+    @animal = Species.find(params[:id])
+    render('species/edit.html.erb')
+  end
+end
