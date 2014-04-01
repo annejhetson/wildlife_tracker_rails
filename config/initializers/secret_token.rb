3ed00a6f14c88b13ba1ff6f865e4863191e4d47d
anne jhetson & Kelly Becker <student@epicodus.com>
2014-04-01 09:13:58 -0700

    start new rails project "wildlife_tracker"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WildlifeTracker::Application.config.secret_key_base = 'ab2fa80184f876a517e698d259f7aa8b32732020900360092bfcad316333ef0ce74328c3968aeb128ca0e6ac7d6f311ea44b642fdb9683ed527b2366ef1fe215'
